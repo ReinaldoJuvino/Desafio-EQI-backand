d661e9d32701301d3aa30aa637b678af3a6ff419
Reinaldo Juvino <reinaldojuvinoneto@gmail.com>
2021-01-28 00:00:21 -0300

    Finalizado

--- knexfile.js
+++ knexfile.js
@@ -15,14 +15,6 @@ module.exports = {
     debug: true,
     ssl: true,
     connection: process.env.DATABASE_URL,
-    // connection: {
-    //     database: process.env.DATABASE_CONECTION_NAME,
-    //     port: process.env.DATABASE_CONECTION_PORT,
-    //     user: process.env.DATABASE_CONECTION_USER,
-    //     password: process.env.DATABASE_CONECTION_PASSWORD,
-    //     host: process.env.DATABASE_CONECTION_HOST,
-    //     ssl: true
-    // },
     pool: {
       min: 2,
       max: 10
