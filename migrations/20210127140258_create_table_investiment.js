5b2d0c3b0201dddc808411007515471bacbfad09
Reinaldo Juvino <reinaldojuvinoneto@gmail.com>
2021-01-28 12:30:12 -0300

    Introdução do arquivo de test do insonmia

--- migrations/20210127140258_create_table_investiment.js
+++ migrations/20210127140258_create_table_investiment.js
@@ -2,10 +2,10 @@ exports.up = function(knex) {
     return knex.schema.createTable('investment', table => {
         table.increments('id').primary();
         table.integer('value',50).notNull();
-        table.string('investment_time',20).notNull();
-        table.string('rate_cdi_percent',50).notNull();
-        table.string('profitability_cdb',50).notNull();
-        table.string('profitability_poupanca',50).notNull();
+        table.integer('investment_time',20).notNull();
+        table.float('rate_cdi_percent',50).notNull();
+        table.integer('profitability_cdb',50).notNull();
+        table.integer('profitability_poupanca',50).notNull();
         table.integer('id_user').references('id').inTable('users');
     })
 };
